2391e6e9c1b68156b038a6c10bd01fed3cfcaa06
ProgroNick <119477833+ProgroNick@users.noreply.github.com>
2023-03-27 09:51:20 +0300

    creat my site

---
+++ main.js
@@ -0,0 +1,23 @@
+function red(el){
+  el.style.cssText = "background-color: red"
+}
+
+function blue(el){
+  el.style.cssText = "background-color: blue"
+}
+
+function yellow(el){
+  el.style.cssText = "background-color: yellow"
+}
+
+function green(el){
+  el.style.cssText = "background-color: green"
+}
+
+function eat(){
+  alert("Я тебе насыпал собачьего корма у твоей будки, ОК?")
+}
+
+function robot(){
+  alert("Да, ты же мой пёсик")
+}
